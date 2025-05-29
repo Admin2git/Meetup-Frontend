d22bf9f644a08ac1817f8c4b05926330c7a61ade
Admin2git <aka62218@gmail.com>
2025-05-29 15:47:12 +0530

    chore : add in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Ensure your app is hosted at the root of the domain or specify a subfolder if needed
+  build: {
+    outDir: "dist", // Output folder for production build
+  },
+  server: {
+    // This is helpful if you're running a local server with React Router's BrowserRouter
+    historyApiFallback: true,
+  },
+});
