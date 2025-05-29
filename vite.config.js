bb013a9e5ed813767b51c64c15749a0a8c1c4d59
Admin2git <aka62218@gmail.com>
2025-05-29 16:22:40 +0530

    fix : vercel and config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure your app is hosted at the root of the domain or specify a subfolder if needed
-  build: {
-    outDir: "dist", // Output folder for production build
-  },
-  server: {
-    // This is helpful if you're running a local server with React Router's BrowserRouter
-    historyApiFallback: true,
-  },
 });
